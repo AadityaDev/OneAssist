5776862bc9f42180e642bcd77a9f0a4f083c238a
Aditya <adityaverma.dev@gmail.com>
2017-12-09 19:30:47 +0530

    dashboard options added

--- app/src/main/java/com/technawabs/oneassist/activities/DashboardActivity.java
+++ app/src/main/java/com/technawabs/oneassist/activities/DashboardActivity.java
@@ -1,11 +1,18 @@
 package com.technawabs.oneassist.activities;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
@@ -21,6 +28,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
 
     private List<Hotel> hotels;
     private List<Hotel> hotelList;
@@ -95,6 +105,12 @@ public class DashboardActivity extends AppCompatActivity {
         initialize();
         readContacts();
         recyclerView.setAdapter(connectionAdapter);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(getActionBar()==null) {
+            setSupportActionBar(toolbar);
+        }
+        initNavigationDrawer();
     }
 
     private void initialize() {
@@ -126,4 +142,52 @@ public class DashboardActivity extends AppCompatActivity {
         connectionAdapter.notifyDataSetChanged();
     }
 
+    public void initNavigationDrawer() {
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                switch (id){
+                    case R.id.home:
+                        Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.settings:
+                        Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.trash:
+                        Toast.makeText(getApplicationContext(),"Trash", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.logout:
+                        finish();
+
+                }
+                return true;
+            }
+        });
+        View header = navigationView.getHeaderView(0);
+        TextView tv_email = (TextView)header.findViewById(R.id.tv_email);
+        tv_email.setText(userDetails.getEmail());
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View v){
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
 }
